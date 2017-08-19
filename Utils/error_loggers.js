8ab8786f3516596aaef3a6c71c98049d7f968c86
Benjamin Hoffmeyer <hoffmeyer25@gmail.com>
2017-08-19 12:00:32 -0400

    QOL for sentry, so errors have release and commit attatched
    also enabled autobreadcrumbs, we'll see how that works out.

--- Utils/error_loggers.js
+++ Utils/error_loggers.js
@@ -3,11 +3,21 @@ const Raven = require('raven')
 
 if (Config.debug === true) {
   Raven.config(Config.services.sentry, {
-    environment: "development"
+    environment: "development",
+    autoBreadcrumbs: true,
+    release: require('./package.json').version,
+    tags: {
+      git_commit: require('git-rev-sync').short()
+    }
   }).install()
 } else {
   Raven.config(Config.services.sentry, {
-    environment: "production"
+    environment: "production",
+    autoBreadcrumbs: true,
+    release: require('./package.json').version,
+    tags: {
+      git_commit: require('git-rev-sync').short()
+    }
   }).install()
 }
 
@@ -16,5 +26,7 @@ module.exports = {
     if (fullErr !== undefined) Raven.captureException(fullErr)
     bot.Channels.find((c) => c.name === 'bot-error').sendMessage(`Encountered an error while trying to run ${cObj.cause}.\nReturned error: \`\`\`${cObj.message}\`\`\``)
   },
-  raven: (error, extra) => Raven.captureException(error, { extra })
+  raven: (error, extra) => Raven.captureException(error, {
+    extra
+  })
 }
